b1580dda4abf97dabfd4f297df6cf802501a3332
Mark Pavel <markpavel02@gmail.com>
2022-06-16 18:55:08 -0400

    Refactored Query Functions

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE employees (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT DEFAULT NULL,
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
     CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- utils/Business.js
+++ utils/Business.js
@@ -1,17 +1,59 @@
-const Employees = require('./Employees');
-const Departments = require('./Departments');
-const Roles = require('./Roles');
-
 module.exports = class Business {
     static async getEmployees(db, params) {
-        return await Employees.getEmployees(db, params);
+        if(!params) {
+            return ( await db.execute('select * from employees;') )[0];
+        }
+
+        const sql = `
+            SELECT
+                employee.id AS ID, CONCAT(employee.first_name, ' ', employee.last_name) AS Employee,
+                roles.title AS Role, CONCAT('$', FORMAT(roles.salary, 2)) AS Salary,
+                departments.name AS Department,
+                IF(employee.manager_id IS NULL, 
+                    'None',
+                    (SELECT
+                        CONCAT(first_name, ' ', last_name)
+                        FROM employees
+                        WHERE id = employee.manager_id
+                    )
+                ) AS Manager
+
+            FROM employees AS employee
+
+            JOIN roles ON employee.role_id = roles.id
+            JOIN departments ON roles.department_id = departments.id
+            JOIN employees AS manager ON employee.id = manager.id
+            ORDER BY employee.id
+        `;
+
+        return ( await db.execute(sql) )[0];
     }
 
     static async getDepartments(db, params) {
-        return await Departments.getDepartments(db, params)
+        if(!params) {
+            return;
+        }
+
+        const sql = `
+            SELECT id AS ID, name AS Name
+            FROM departments
+        `;
+
+        return ( await db.execute(sql) )[0];
     }
 
     static async getRoles(db, params) {
-        return await Roles.getRoles(db, params)
+        if(!params) {
+            return;
+        }
+
+        const sql = `
+            SELECT title AS Title, CONCAT('$', FORMAT(salary, 2)) AS Salary, name AS Department
+            FROM roles
+
+            JOIN departments ON roles.department_id = departments.id
+        `;
+
+        return ( await db.execute(sql) )[0];
     }
 }
